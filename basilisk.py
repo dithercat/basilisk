c3e081125f88337e4b0e70041082eaceaf6cfbe4
dither <ditheredlights@gmail.com>
2023-06-01 17:33:25 -0400

    clean things up

--- basilisk.py
+++ basilisk.py
@@ -16,29 +16,30 @@ torch.cuda._lazy_init()
 
 parser = argparse.ArgumentParser(description = "a very simple API server based on ExLlama")
 
-parser.add_argument("-t", "--tokenizer", type = str, help = "Tokenizer model path")
-parser.add_argument("-c", "--config", type = str, help = "Model config path (config.json)")
-parser.add_argument("-m", "--model", type = str, help = "Model weights path (.pt or .safetensors file)")
-parser.add_argument("-d", "--directory", type = str, help = "Path to directory containing config.json, model.tokenizer and * .safetensors")
+parser.add_argument("-t", "--tokenizer", type = str, help = "tokenizer model path")
+parser.add_argument("-c", "--config", type = str, help = "model config path (config.json)")
+parser.add_argument("-m", "--model", type = str, help = "model weights path (.pt or .safetensors file)")
+parser.add_argument("-d", "--directory", type = str, help = "path to directory containing config.json, model.tokenizer and * .safetensors")
 
-parser.add_argument("-a", "--attention", type = ExLlamaConfig.AttentionMethod.argparse, choices = list(ExLlamaConfig.AttentionMethod), help="Attention method", default = ExLlamaConfig.AttentionMethod.SWITCHED)
-parser.add_argument("-mm", "--matmul", type = ExLlamaConfig.MatmulMethod.argparse, choices = list(ExLlamaConfig.MatmulMethod), help="Matmul method", default = ExLlamaConfig.MatmulMethod.SWITCHED)
-parser.add_argument("-mlp", "--mlp", type = ExLlamaConfig.MLPMethod.argparse, choices = list(ExLlamaConfig.MLPMethod), help="Matmul method", default = ExLlamaConfig.MLPMethod.SWITCHED)
-parser.add_argument("-s", "--stream", type = int, help = "Stream layer interval", default = 0)
-parser.add_argument("-gs", "--gpu_split", type = str, help = "Comma-separated list of VRAM (in GB) to use per GPU device for model layers, e.g. -gs 20,7,7")
-parser.add_argument("-dq", "--dequant", type = str, help = "Number of layers (per GPU) to de-quantize at load time")
+parser.add_argument("-a", "--attention", type = ExLlamaConfig.AttentionMethod.argparse, choices = list(ExLlamaConfig.AttentionMethod), help="attention method", default = ExLlamaConfig.AttentionMethod.SWITCHED)
+parser.add_argument("-mm", "--matmul", type = ExLlamaConfig.MatmulMethod.argparse, choices = list(ExLlamaConfig.MatmulMethod), help="matmul method", default = ExLlamaConfig.MatmulMethod.SWITCHED)
+parser.add_argument("-mlp", "--mlp", type = ExLlamaConfig.MLPMethod.argparse, choices = list(ExLlamaConfig.MLPMethod), help="matmul method", default = ExLlamaConfig.MLPMethod.NORMAL)
+parser.add_argument("-s", "--stream", type = int, help = "stream layer interval", default = 0)
+parser.add_argument("-gs", "--gpu_split", type = str, help = "comma-separated list of VRAM (in GB) to use per GPU device for model layers, e.g. -gs 20,7,7")
+parser.add_argument("-dq", "--dequant", type = str, help = "number of layers (per GPU) to de-quantize at load time")
 
-parser.add_argument("-temp", "--temperature", type = float, help = "Temperature", default = 0.5)
-parser.add_argument("-topk", "--top_k", type = int, help = "Top-K", default = 32)
-parser.add_argument("-topp", "--top_p", type = float, help = "Top-P", default = 0.2)
-parser.add_argument("-minp", "--min_p", type = float, help = "Min-P", default = 0.00)
-parser.add_argument("-repp",  "--repetition_penalty", type = float, help = "Repetition penalty", default = 1.1)
-parser.add_argument("-repps", "--repetition_penalty_sustain", type = int, help = "Past length for repetition penalty", default = 32)
-parser.add_argument("-repdc", "--repetition_penalty_decay", type = int, help = "Decay length for repetition penalty", default = 64)
-parser.add_argument("-beams", "--beams", type = int, help = "Number of beams for beam search", default = 1)
-parser.add_argument("-beamlen", "--beam_length", type = int, help = "Number of future tokens to consider", default = 1)
+parser.add_argument("-temp", "--temperature", type = float, help = "temperature", default = 0.5)
+parser.add_argument("-topk", "--top_k", type = int, help = "top-k", default = 32)
+parser.add_argument("-topp", "--top_p", type = float, help = "top-p", default = 0.2)
+parser.add_argument("-minp", "--min_p", type = float, help = "min-p", default = 0.00)
+parser.add_argument("-repp",  "--repetition_penalty", type = float, help = "repetition penalty", default = 1.1)
+parser.add_argument("-repps", "--repetition_penalty_sustain", type = int, help = "past length for repetition penalty", default = 32)
+parser.add_argument("-repdc", "--repetition_penalty_decay", type = int, help = "decay length for repetition penalty", default = 64)
+parser.add_argument("-repat", "--positional_repetition_penalty", type = int, help = "positional repetition penalty", default = 1.2)
+parser.add_argument("-beams", "--beams", type = int, help = "number of beams for beam search", default = 1)
+parser.add_argument("-beamlen", "--beam_length", type = int, help = "number of future tokens to consider", default = 1)
 
-parser.add_argument("-gpfix", "--gpu_peer_fix", action = "store_true", help = "Prevent direct copies of data between GPUs")
+parser.add_argument("-gpfix", "--gpu_peer_fix", action = "store_true", help = "prevent direct copies of data between GPUs")
 
 cargs = parser.parse_args()
 
@@ -64,6 +65,7 @@ config.update({
     "repetition_penalty": cargs.repetition_penalty,
     "repetition_penalty_sustain": cargs.repetition_penalty_sustain,
     "repetition_penalty_decay": cargs.repetition_penalty_decay,
+    "positional_repeat_penalty": cargs.positional_repetition_penalty,
     "beams": cargs.beams,
     "beam_length": cargs.beam_length,
     "gpu_peer_fix": cargs.gpu_peer_fix
@@ -130,22 +132,13 @@ generator.settings.token_repetition_penalty_decay = config["repetition_penalty_d
 generator.settings.beams = config["beams"]
 generator.settings.beam_length = config["beam_length"]
 
-def searcharr(haystack, needle, start=0):
-    for i in range(start, len(haystack) - len(needle) + 1):
-        found = True
-        for j in range(0, len(needle)):
-            if haystack[i + j] != needle[j]:
-                found = False
-                break
-        if found:
-            return i
-    return None
-
+# HACK: replace literal <s> and </s> with STX and ETX
+#       and then replace STX and ETX with BOS and EOS
 bos_tok = tokenizer.tokenizer.Encode("\x02")[1]
 eos_tok = tokenizer.tokenizer.Encode("\x03")[1]
-newline_tok = tokenizer.tokenizer.Encode("\n")[1]
 def tokenize_evil(str):
-    ids = tokenizer.tokenizer.Encode(str.replace("<s>", "\x02").replace("</s>", "\x03"))
+    ids = tokenizer.tokenizer.Encode(
+        str.replace("<s>", "\x02").replace("</s>", "\x03"))
     for i in range(0, len(ids)):
         if ids[i] == bos_tok:
             ids[i] = tokenizer.bos_token_id
@@ -153,20 +146,51 @@ def tokenize_evil(str):
             ids[i] = tokenizer.eos_token_id
     return ids
 
+# actual app init
 app = Flask(__name__)
 
+# validate shared secret
+def auth_check():
+    secret = config.get("shared_secret")
+    return secret != None and request.headers.get("authorization") != secret
+
+# dedicated "see if the config is working" endpoint
+# maps to kasumiLLM's ping()
+# (which used to be a hack using textgen-webui token count endpoint)
 @app.route("/basilisk/ping")
 def get_ping():
-    secret = config.get("shared_secret")
-    if secret != None and request.headers.get("authorization") != secret:
+    if auth_check():
         return "unauthorized", 403
-    
     return "pong"
 
+# allow clients to retrieve most of the base config
+@app.route("/basilisk/config")
+def get_config():
+    if auth_check():
+        return "unauthorized", 403
+    
+    cfg = {}
+    cfg.update(config)
+
+    # these dont serialize
+    del cfg["attention"]
+    del cfg["matmul"]
+    del cfg["mlp"]
+
+    # dont disclose these
+    del cfg["shared_secret"]
+    del cfg["directory"]
+    del cfg["config"]
+    del cfg["model"]
+    del cfg["tokenizer"]
+
+    return cfg
+
+# tokenize a string
+# primary use is to get token count, but also for positional_repeat_inhibit
 @app.route("/basilisk/tokenize", methods=["POST"])
 def post_tokens():
-    secret = config.get("shared_secret")
-    if secret != None and request.headers.get("authorization") != secret:
+    if auth_check():
         return "unauthorized", 403
     
     body = request.get_json()
@@ -178,12 +202,14 @@ def post_tokens():
         "tokens": ids
     }
 
+# actual inference endpoint
+# param names are kind of sort of compatible with text-generation-webui
 @app.route("/basilisk/infer", methods=["POST"])
 def post_infer():
-    secret = config.get("shared_secret")
-    if secret != None and request.headers.get("authorization") != secret:
+    if auth_check():
         return "unauthorized", 403
     
+    # get parameters+overrides
     body = {
         "min_length": 4,
         "max_new_tokens": 256,
@@ -191,7 +217,7 @@ def post_infer():
 
         # providing the last inference here attempts to prevent any token from
         # being generated at the same position as in the previous sequence
-        "positional_repeat_penalty": 1.3,
+        "positional_repeat_penalty": 1.2,
         "positional_repeat_inhibit": []
     }
     body.update(config)
@@ -237,7 +263,7 @@ def post_infer():
         # Disallowing the end condition tokens seems like a clean way to force longer replies.
         if i < body["min_length"]:
             generator.disallow_tokens([
-                newline_tok,
+                tokenizer.newline_token_id,
                 tokenizer.eos_token_id
             ])
         else:
@@ -248,16 +274,21 @@ def post_infer():
 
         # stop on stopping strings
         stophit = False
-        for string in stopping_strings_tok:
-            haystack = generator.sequence[0][initial_len:][-len(string):].tolist()
-            if searcharr(haystack, string) != None:
+        for needle in stopping_strings_tok:
+            haystack = generator.sequence[0][initial_len:][-len(needle):].tolist()
+            found = True
+            for j in range(0, len(needle)):
+                if haystack[j] != needle[j]:
+                    found = False
+                    break
+            if found:
                 stophit = True
         if stophit:
             break
 
         # If token is EOS, replace it with newline before continuing
         if token.item() == tokenizer.eos_token_id:
-            generator.replace_last_token(newline_tok)
+            generator.replace_last_token(tokenizer.newline_token_id)
             break
     
     tokens = generator.sequence[0][initial_len:]
